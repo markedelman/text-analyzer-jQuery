3d3d1e56da354e9af376633f351b15965b7c50f4
Mark Edelman <markedelman1987@gmail.com>
2017-03-13 01:35:22 -0400

    final setup

--- app.js
+++ app.js
@@ -40,33 +40,29 @@ function averageWordLength(input) {
 // jQuery DOM Functions 1,2,3,4,5
 
 // jQuery DOM Function 1
-// Declares input as value entered into "textarea"
-// var userInput = $(".js-text-form").find('#user-text').val();
-
-
-// jQuery DOM Function 2
-// Displays wordCount result into "<dt>Word count</dt><dd></dd>"
-// $(".js-wordCount").append(`${convertText(userInput)}`);
-
-// jQuery DOM Function 3
-// Displays uniqueWordCount result into
-// "<dt>Unique word count</dt><dd></dd>"
-
-// $(".js-uniqueWordCount").append('value');
-
-// jQuery DOM Function 4
-// Displays averageWordLength result into
-// "<dt>Average word length</dt>
-// <dd></dd>"
-// $(".js-averageWordLength").append(`{$value}`);
-
-// jQuery DOM Function 5
 // Listens for click on "<button type="submit">Analyze it!</button>"
 $('.button').click(function() {
     event.preventDefault();
+
+    // jQuery DOM Function 2
+    // removes 'hidden' class from <dl>
     $('dl').removeClass('hidden');
+
+    // jQuery DOM Function 3
+    // Declares 'userInput' variable to the input text in the 'textarea' tag
     var userInput = $(".js-text-form").find('#user-text').val();
+
+    // jQuery DOM Function 3
+    // Displays wordCount result into "<dt>Word count</dt><dd></dd>"
     $(".js-wordCount").append(`${convertText(userInput).length}`);
+
+    // jQuery DOM Function 4
+    // Displays uniqueWordCount result into
+    // "<dt>Unique word count</dt><dd></dd>"
     $(".js-uniqueWordCount").append(`${uniqueWordCounter(userInput)}`);
+
+    // jQuery DOM Function 5
+    // Displays averageWordLength result into
+    // "<dt>Average word length</dt><dd></dd>"
     $(".js-averageWordLength").append(`${averageWordLength(userInput)}`);
 });
